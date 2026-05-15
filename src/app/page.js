8b9bb15578f02e6e063907d4c354364550bca89e
svengraffiti-cloud <svengraffiti@gmail.com>
2026-05-15 14:06:41 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -661,11 +661,11 @@ className={`rounded-3xl border p-6 ${
                             Valider
                           </button>
                         </div>
-                      </div>
-                   )}
-                  
+ )}
+</div>
 </section>
-            )}            {tab === "classement" && (
+
+{tab === "classement" && (
               <section className="rounded-3xl border border-[#3b2458] bg-[#211433]/90 p-6">
                 <h2 className="mb-5 flex items-center gap-2 text-2xl font-bold">
                   <Trophy className="h-6 w-6 text-yellow-400" />
