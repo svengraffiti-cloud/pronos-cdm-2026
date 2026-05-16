1e88af29acf7dba9999f108861dfefd4e414a0e8
svengraffiti-cloud <svengraffiti@gmail.com>
2026-05-16 16:10:20 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -38,7 +38,7 @@ function AppShell({ children }) {
 function AppLogo({ className = "", alt = "Logo Pronos Famille" }) {
   return (
     <img
-      src="/papy-logo-v2.png"
+      src="/logo-v2.png"
       alt={alt}
       className={className}
       loading="eager"
