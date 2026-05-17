8cc3bbf1f9667d8328463ae87bde7eac79f53373
SVEN <sven@macbook-pro-de-sven.home>
2026-05-17 23:12:08 +0200

    Fix classement et synchro live

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-<<<<<<< HEAD
   appId: 'com.lespronosdepapy.app',
   appName: 'Les Pronos de Papy',
-  webDir: 'public',
+  webDir: 'out',
   server: {
     url: 'https://www.lespronosdepapy.com',
     cleartext: false
   }
-=======
-  appId: 'com.pronospapy.app',
-  appName: 'Les Pronos de Papy',
-  webDir: 'out'
->>>>>>> 576ffa1 (Ajout bouton rafraichir app)
 };
 
 export default config;
