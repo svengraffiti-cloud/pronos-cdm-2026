eca79e6ccc39f31519511d2ad6eeccecac056628
SVEN <sven@macbook-pro-de-sven.home>
2026-05-17 21:13:57 +0200

    fix android native notifications build

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: {
     unoptimized: true,
   },
-=======
   output: "export",
->>>>>>> 576ffa1 (Ajout bouton rafraichir app)
 };
 
 export default nextConfig;
